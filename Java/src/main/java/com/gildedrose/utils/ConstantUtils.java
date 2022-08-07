370d2350c018e22e9a6e765ce67478ced1005c73
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-07 21:32:00 +0200

    Separate functions to manage updateQuality logic Refactor magic numbers Refactor frequent Conditions into methods

---
+++ Java/src/main/java/com/gildedrose/utils/ConstantUtils.java
@@ -0,0 +1,15 @@
+package com.gildedrose.utils;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public interface ConstantUtils {
+
+    final String NORMAL = "foo";
+    final String BRIE = "Aged Brie";
+    final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    final int MAX_QUALITY = 50;
+    final int MAX_REMINING_DAYS = 11;
+    final int MIN_REMINING_DAYS = 6;
+    final int ZERO = NumberUtils.BYTE_ZERO;
+}
