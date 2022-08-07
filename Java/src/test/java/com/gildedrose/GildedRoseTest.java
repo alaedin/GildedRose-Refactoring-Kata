e57e2740ce159bc5b6c4fd916ca202f8be4d81a4
alaeldin <arfaoui.ala.eddin@gmail.com>
2022-08-07 20:08:54 +0200

    Add unit tests to maintain the existing behavior

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,45 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.lambda.utils.Range;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GildedRoseTest {
 
+    private String NORMAL;
+    private String BRIE;
+    private String BACKSTAGE;
+    private String SULFURAS;
+
+    @BeforeAll
+    void init() {
+        NORMAL = "foo";
+        BRIE = "Aged Brie";
+        BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+        SULFURAS = "Sulfuras, Hand of Ragnaros";
+    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void updateQuality() {
+        Integer[] sellInRange = Range.get(-1, 11);
+        Integer[] qualityRange = Range.get(0, 50);
+        String[] names = {NORMAL, BRIE, BACKSTAGE, SULFURAS,};
+
+        CombinationApprovals.verifyAllCombinations(
+            this::doUpdateQuality,
+            names,
+            sellInRange,
+            qualityRange);
+    }
+
+    private Item doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        return gildedRose.items[0];
     }
 
 }
